93aee5693ffa70aa4be5b1d9a382f024ad2e0126
Mehul Verma <34917916+Mehul266@users.noreply.github.com>
2020-10-01 14:00:11 +0530

    Update voiceDetection.py
    listen for name

--- voiceDetection.py
+++ voiceDetection.py
@@ -1,12 +1,10 @@
 import speech_recognition as sr
 
-r1 = sr.Recognizer()
-r2 = sr.Recognizer()
-
+r = sr.Recognizer()
 
 with sr.Microphone() as source:
     print('Started:')
-    audio = r2.listen(source)
+    audio = r.listen(source)
 
-if 'facebook' in r1.recognize_google(audio):
+if 'Mehul' in r.recognize_google(audio):
     print('detected')
